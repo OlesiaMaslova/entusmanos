2c700c7210b6bc7a5ae4888dfd26eef66a703480
Olesia Maslova <qwadronum@yandex.ru>
2023-08-17 00:06:08 -0400

    header main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
+import { Route, useHistory} from 'react-router-dom';
 import './App.css';
 import '../../vendor/normalize.css';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
 
 function App() {
   return (
     <div className="App">
+    <Header/>
+    <Main/>
+
+
       
 
 
